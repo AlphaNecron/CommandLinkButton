6fd9a8402db6e0cc231b7b70353c7fbeac1f93c8
AlphaNecron <57827456+AlphaNecron@users.noreply.github.com>
2021-04-20 20:54:15 +0700

    Initial commit

---
+++ CommandLinkButton.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Windows.Forms.CommandLinkButton
+{
+    public partial class CommandLinkButton : Button
+    {
+        private const int BS_COMMANDLINK = 0x0000000E;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cParams = base.CreateParams;
+                cParams.Style |= BS_COMMANDLINK;
+                return cParams;
+            }
+        }
+        public CommandLinkButton()
+        {
+            InitializeComponent();
+            this.FlatStyle = FlatStyle.System;
+        }
+    }
+}
